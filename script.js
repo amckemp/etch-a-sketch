c53529f4b10be82e289071d7521f1e707db7d6e3
amckemp <amc.kemp@gmail.com>
2023-03-09 12:26:19 -0500

    Create grid that changes size relative to slider input

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
+const DEFAULT_GRID_SIZE = 16;
+
+// Create initial 16 x 16 grid
 const body = document.querySelector('body');
-const grid = document.createElement('div');
-grid.classList.add('grid');
-body.appendChild(grid);
+const grid = document.querySelector('.grid');
+
+const sliderValue = document.querySelector('.sliderValue');
+const sliderInput = document.querySelector('.slider');
+let gridSize = DEFAULT_GRID_SIZE;
+
+createGrid (16);
+console.log('initial grid created');
+
+// Create grid w slider
+sliderInput.addEventListener("input", (event) => {
+    clear();
+    createGrid(event.target.value);
+});
 
 // Create grid
-for (let i = 0; i < 16; i++){
-    const row = document.createElement('div');
-    row.classList.add('row');
-    grid.appendChild(row);
-    for (let j = 0; j < 16; j++){
-        const div = document.createElement('div');
-        div.classList.add('pixel');
-        row.appendChild(div);
-    }
-}
+function createGrid (gridSize) {
+    sliderValue.textContent = `${gridSize} x ${gridSize}`;
 
+    grid.innerHTML = '';
+
+    for (let i = 0; i < gridSize; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        grid.appendChild(row);
+        for (let j = 0; j < gridSize; j++){
+            const div = document.createElement('div');
+            div.classList.add('pixel');
+            row.appendChild(div);
+            div.style.width = `${500/gridSize}px`;
+            div.style.height = `${500/gridSize}px`;
+        }
+    }
+};
 
 // Add colour to pixel of grid when user hovers over
 const pixels = document.querySelectorAll('.pixel');
