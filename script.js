1beac20198047de56bb64c68da00ce6a4e563765
amckemp <amc.kemp@gmail.com>
2023-03-09 11:16:20 -0500

    Create basic etch-a-sketch (grid, clear button, hover over to colour in pixels)

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-
 const body = document.querySelector('body');
 const grid = document.createElement('div');
+grid.classList.add('grid');
 body.appendChild(grid);
 
+// Create grid
 for (let i = 0; i < 16; i++){
     const row = document.createElement('div');
     row.classList.add('row');
@@ -12,4 +13,25 @@ for (let i = 0; i < 16; i++){
         div.classList.add('pixel');
         row.appendChild(div);
     }
+}
+
+
+// Add colour to pixel of grid when user hovers over
+const pixels = document.querySelectorAll('.pixel');
+
+pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseover', () => {
+        pixel.classList.add('colour');
+    });
+});
+
+
+// Clear grid (clear all pixels)
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', clear);
+
+function clear () {
+        pixels.forEach((pixel) => {
+            pixel.classList.remove('colour');
+        });
 }
